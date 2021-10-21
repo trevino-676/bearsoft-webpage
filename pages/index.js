daaf6a9b24d41fc7d6fafd2afc709a13a963d117
Luis Manuel Torres Trevino <lmtorres123@icloud.com>
2021-10-20 23:28:37 -0500

    Create the index header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
+import {Container, Box, Heading} from '@chakra-ui/react'
+
 const Page = () => {
 	return (
-		<div>Hello</div>
+		<Container>
+			<Box borderRadius="lg" background="red" p={3} align="center">
+				Hello, I&apos;m a full-stack developer from Mexico
+			</Box>
+			<Box display={{md: 'flex'}}>
+				<Box flexGrow={1}>
+					<Heading as="h2" variant="page-title">
+						Luis Manuel Torres Trevino
+					</Heading>
+					<p>Sofware Developer (Frontend, Backend, Data)</p>
+				</Box>
+			</Box>
+		</Container>
 	)
 }
 
